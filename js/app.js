500585bddfb77fa86b50054c36fb378dc61c0ffb
Jeff Ripke <jripke74@gmail.com>
2017-09-14 07:38:57 -0700

    DOMTraversal: reset form

--- js/app.js
+++ js/app.js
@@ -8,19 +8,24 @@ $('.loc').hover(
 
 // Grab info from the form
 $('#add-pet').on('click', () => {
-const $name = $('#pet-name');
-const $species = $('#pet-species');
-const $notes = $('#pet-notes');
+  const $name = $('#pet-name');
+  const $species = $('#pet-species');
+  const $notes = $('#pet-notes');
 
-var $newPet = $(
-  '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
-  '</p><p><strong>Species:</strong> ' + $species.val() +
-  '</p><p><strong>Notes:</strong> ' + $species.val() +
-  '</p><span class="close">&times;</span></div></section>'
-);
+  var $newPet = $(
+    '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
+    '</p><p><strong>Species:</strong> ' + $species.val() +
+    '</p><p><strong>Notes:</strong> ' + $species.val() +
+    '</p><span class="close">&times;</span></div></section>'
+  );
 
-// Attach the new element to the page
-$('#posted-pets').append($newPet);
+  // Attach the new element to the page
+  $('#posted-pets').append($newPet);
+
+  // Reset form fields
+  $name.val("");
+  $species.val("Dog");
+  $notes.val("");
 });
 
 // puppy images fade in
